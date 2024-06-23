400b651ac4ee725dccc8c173006565e68fe03434
Wayne <46545469+wkrose7@users.noreply.github.com>
2024-06-23 14:09:26 -0400

    Wayne's Content Studio Website
    A WIP.

---
+++ waynescontentstudio/process_form.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // You can add email sending functionality here
+    // For example, using PHP's mail() function
+
+    echo "Thank you, $name. Your message has been received.";
+} else {
+    echo "There was an error processing your form. Please try again.";
+}
+
+?>
